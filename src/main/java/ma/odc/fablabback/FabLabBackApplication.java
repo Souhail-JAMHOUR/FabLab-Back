b3047c457f333fe109016530b211721bfe72a5aa
Souhail <souhail.edu@gmail.com>
2023-08-07 12:11:26 +0100

    first commint

---
+++ src/main/java/ma/odc/fablabback/FabLabBackApplication.java
@@ -0,0 +1,13 @@
+package ma.odc.fablabback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabLabBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabLabBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/odc/fablabback/FabLabBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.odc.fablabback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabLabBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
