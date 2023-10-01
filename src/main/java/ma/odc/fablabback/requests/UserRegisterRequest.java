399f7648d9c9f0779e923ff0d085efddc92a3e40
Souhail <souhail.edu@gmail.com>
2023-10-01 22:05:12 +0100

    modify to return pages closes#7

--- src/main/java/ma/odc/fablabback/requests/UserRegisterRequest.java
+++ src/main/java/ma/odc/fablabback/requests/UserRegisterRequest.java
@@ -1,13 +1,9 @@
 package ma.odc.fablabback.requests;
 
-import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ma.odc.fablabback.enums.Sex;
 
 @Data
 @NoArgsConstructor
@@ -19,13 +15,13 @@ public class UserRegisterRequest {
   private String email;
   private String password;
   private String confirmedPassword;
-//  private String cin;
-//
-//  @JsonFormat(pattern = "yyyy-MM-dd")
-//  private LocalDate birthDate;
-//
-//  @JsonEnumDefaultValue private Sex sex;
-//  private String status;
-//
-//  private String etablissment;
+  //  private String cin;
+  //
+  //  @JsonFormat(pattern = "yyyy-MM-dd")
+  //  private LocalDate birthDate;
+  //
+  //  @JsonEnumDefaultValue private Sex sex;
+  //  private String status;
+  //
+  //  private String etablissment;
 }
